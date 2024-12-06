084f9b797b890caf0da855db06dff6869d7ca56a
Priyesh <princekathiriya28@gmail.com>
2024-12-06 22:31:52 +0530

    first branch commit

--- lib/app/view/login/login_screen.dart
+++ lib/app/view/login/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:invoice_app/helper/constant/color_data.dart';
+import 'package:invoice_app/helper/widget/widget_utils.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,6 +13,10 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: getCustomFont("Login",16.sp, textColor, 1),
+      ),
+    );
   }
 }
